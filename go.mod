149d57b6cf187304f14d46c6010ff22ac0d68f42
hanyue1014 <wongxuanbin@gmail.com>
2022-01-14 21:19:54 +0800

    Successfully got a window on screen

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hanyue1014/deskpet
 
 go 1.17
+
+require github.com/veandco/go-sdl2 v0.4.11 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
+	// "github.com/veandco/go-sdl2/img"
+)
+
+const (
+	windowWidth  = 200
+	windowHeight = 200
 )
 
 func main() {
 	fmt.Println("Hello from go")
+
+	sdl.Init(sdl.INIT_EVERYTHING)
+	// quit sdl to prevent memory leak
+	defer sdl.Quit()
+
+	w, r, err := sdl.CreateWindowAndRenderer(windowWidth, windowHeight, sdl.WINDOW_ALWAYS_ON_TOP)
+	// close window and renderer properly
+	defer w.Destroy()
+	defer r.Destroy()
+
+	if err != nil {
+		panic(fmt.Sprintf("cannot initialise window or renderer %v", err))
+	}
+
+	time.Sleep(5 * time.Second)
+
+	// img.Init(img.INIT_PNG)
 }
